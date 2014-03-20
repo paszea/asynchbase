913822652bea53cc4b583f5fa5e0dac4bf986341
Xun Liu <xun@pinterest.com>
2014-03-20 16:43:40 -0700

    fix

--- src/HBaseClient.java
+++ src/HBaseClient.java
@@ -34,7 +34,6 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
